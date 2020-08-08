ced84ac2a736217b4e6fd7745ed52e9f724d4192
Mahmood Nihad <m.zibari@gmail.com>
2020-08-08 17:32:53 -0500

    added console.logs

--- src/app.js
+++ src/app.js
@@ -20,10 +20,10 @@ app.use('/api/users', usersRouter)
 
 app.use(function errorHandler(error, req, res, next) {
     let response
+    console.error(error)
     if (NODE_ENV === 'production') {
         response = { error: { message: 'server error' } }
     } else {
-        console.error(error)
         response = { message: error.message, error }
     }
     res.status(500).json(response)
