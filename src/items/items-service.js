b0f84722488c114c0bd3d33e1c41f1b5901bd9d5
Mahmood Nihad <m.zibari@gmail.com>
2020-08-15 20:54:29 -0500

    fixed a bug in /api/items endpoint

--- src/items/items-service.js
+++ src/items/items-service.js
@@ -1,6 +1,15 @@
 const ItemsService = {
     getAllItems(knex) {
-        return knex.select('*').from('items')
+        return knex
+        .select(
+            'items.id AS id',
+            'items.item_name',
+            'quantity',
+            'item_type.item_name AS type',
+            'expiration',
+        )
+        .from('items')
+        .join('item_type', 'items.item_type', '=', 'item_type.id')
     },
 
 }
